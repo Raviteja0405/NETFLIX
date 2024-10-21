d4402d6de8fc1ac514329292a07e52145773e2b0
Raviteja0405 <raviteja40025040@gmail.com>
2024-10-21 20:29:42 +0530

    backend controllers

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 import express from "express";
+
 import authRoutes from "./routes/auth.route.js";
+import { ENV_VARS } from "./config/envVars.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 app.use(express.json());
-
-const PORT = process.env.PORT || 5000;
+const PORT = ENV_VARS.PORT;
 
 app.use("/api/v1/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+  connectDB();
 });
