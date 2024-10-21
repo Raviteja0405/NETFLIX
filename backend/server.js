9ac4f5711a5c92c0d163e3d7a5be671bba032cea
Raviteja0405 <raviteja40025040@gmail.com>
2024-10-21 20:03:43 +0530

    before setting up auth

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,12 @@
 import express from "express";
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use("/api/v1/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
