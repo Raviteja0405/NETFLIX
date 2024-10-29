b952112ec07aea021e724552618abcbe875210f4
Raviteja0405 <raviteja40025040@gmail.com>
2024-10-29 20:30:59 +0530

    add trending movies and trailers

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -12,3 +12,30 @@ export async function getTrendingMovie(req, res) {
     res.status(500).json({ success: false, message: "Internal server error" });
   }
 }
+
+export async function getMovieTrailers(req, res) {
+  const { id } = req.params;
+  try {
+    const data = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`
+    );
+    res.status(200).json({ success: true, trailers: data.results });
+  } catch (error) {
+    if (error.message.includes("404")) {
+      return res.status(404).send(null);
+    }
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+}
+
+export async function getMovieDetails(req, res) {
+  const { id } = req.params;
+  try {
+    const data = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/movie/${id}?language=en-US`
+    );
+    res.status(200).json({ success: true, details: data });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+}
--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { getTrendingMovie } from "../controllers/movie.controller.js";
+import { getTrendingMovie, getMovieTrailers , getMovieDetails} from "../controllers/movie.controller.js";
 
 const router = express.Router();
 
 router.get("/trending", getTrendingMovie);
+router.get("/:id/trailers", getMovieTrailers);
+router.get("/:id/details", getMovieDetails);
 
 export default router;
